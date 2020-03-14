b0ab2aa74883a8afe6272e21b57b72ce9c0bd6fe
johan <johan@DESKTOP-P3RFRER>
2020-03-13 19:55:29 -0600

    primeer commit

---
+++ src/Johansel_Chaves/NewMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Johansel_Chaves;
+
+/**
+ *
+ * @author johan
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //puto el que lo lea
+    }
+    
+}
